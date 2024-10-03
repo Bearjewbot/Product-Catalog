3c8087058dbeb6535faebdfed07eff1f429555e3
Daniel Lovened <daniel_lovened@hotmail.com>
2024-10-03 22:10:57 +0200

    Fixed the layout. Now the code behind needs to be fixed.

--- Maui.Client/MainPage.xaml.cs
+++ Maui.Client/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using SampleApp.Library.Services;
+﻿using SampleApp.Library.Models;
+using SampleApp.Library.Services;
 
 namespace Maui.Client;
 
@@ -9,28 +10,40 @@ public partial class MainPage : ContentPage
 
     ProductService productservice = new();
 
-       
+
 
     public MainPage()
     {
         InitializeComponent();
 
-            
 
-            
-        listContacts.ItemsSource = productservice.GetAllProducts();
-            
+        List<Product> products = new() {new Product
+        {
+            Name = "John",
+            Price = 300
+
+
+        }};
+
+        listContacts.ItemsSource = products;
+        //listContacts.ItemsSource = productservice.GetAllProducts();
+
     }
 
     private void btnAddProduct_Clicked(object sender, EventArgs e)
     {
 
-        if (input_Name.Text != null && input_Price.Text != null!) 
+        if (input_Name.Text != null && input_Price.Text != null!)
         {
             //productservice.AddProduct(input_Name.Text, input_Price.Text); 
 
         }
 
+
+    }
+
+    private void listContacts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
 
     }
 }
